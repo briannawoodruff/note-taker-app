c889df22545f81d3f2ce2c8328569038a7ae8514
Brianna Woodruff <briannaewoodruff@gmail.com>
2021-12-12 00:32:34 -0600

    route issue

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 // GET * should return the index.html file.
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // GET /notes should return the notes.html file.
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
