751930f755c6570ff1b4b0146bacfb732651e926
Brianna Woodruff <briannaewoodruff@gmail.com>
2021-12-12 00:22:38 -0600

    initial

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // GET * should return the index.html file.
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
